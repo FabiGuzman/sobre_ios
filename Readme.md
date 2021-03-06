acace93b255397543069d21bb2aa7e86af345545
Fabian <fabian.guzman@avaldigitallabs.com>
2021-03-06 12:03:34 -0500

    [feature/ios-20210306] backup RestaurantList cap_21

--- Readme.md
+++ Readme.md
@@ -1,1 +1,2 @@
 #   Add code for ios
+#   Add code for show photo library of device in ImageView Object
--- RestaurantList/RestaurantList/AddTableViewController.swift
+++ RestaurantList/RestaurantList/AddTableViewController.swift
@@ -1,7 +1,9 @@
 import UIKit
 
-class AddTableViewController: UITableViewController {
+class AddTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
@@ -10,12 +12,20 @@ class AddTableViewController: UITableViewController {
         if indexPath.row == 0 {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary){
             let imagePicker = UIImagePickerController()
+            imagePicker.delegate = self
             imagePicker.allowsEditing = false
             imagePicker.sourceType = .photoLibrary
             
         self.present(imagePicker, animated: true, completion: nil)
           }
          }
+    }
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        imageView.image = (info[UIImagePickerController.InfoKey.originalImage] as! UIImage)
+        imageView.contentMode = UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        dismiss(animated: true, completion: nil)
     }
     
 }
